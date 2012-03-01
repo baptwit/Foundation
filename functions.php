27473ee178b60c3c074bb368616c465f91c28bcc
Baptiste Caquot <baptistecaquot@gmail.com>
2012-03-01 15:30:57 +0100

    adding auto_compile_less function

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-
+//include LESSphp
+include "lessphp/lessc.inc.php";
 // Disable WordPress version reporting as a basic protection against attacks
 function remove_generators() {
 	return '';
@@ -305,5 +306,20 @@ function emm_paginate_loop($start, $max, $page = 0) {
 	return $output;
 } 
 
+function auto_compile_less($less_fname, $css_fname) {
+  // load the cache
+  $cache_fname = $less_fname.".cache";
+  if (file_exists($cache_fname)) {
+    $cache = unserialize(file_get_contents($cache_fname));
+  } else {
+    $cache = $less_fname;
+  }
+
+  $new_cache = lessc::cexecute($cache);
+  if (!is_array($cache) || $new_cache['updated'] > $cache['updated']) {
+    file_put_contents($cache_fname, serialize($new_cache));
+    file_put_contents($css_fname, $new_cache['compiled']);
+  }
+}
 
 ?>
